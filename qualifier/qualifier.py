23d280944b77c2b97fe2c447b5dd82d042ce24f0
richard <richardphi1618@gmail.com>
2021-07-22 20:01:55 -0400

    pyrva code night review

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -1,8 +1,14 @@
 from typing import Any, List, Optional
 import copy
 
+TOP = "┌┬┐"
+MID = "├┼┤"
+BOT = "└┴┘"
+BAR = "│"
+LINE = "─"
 
 def join_l(l, sep):
+    """Join list with seperator"""
     li = iter(l)
     string = str(next(li))
     for i in li:
@@ -12,8 +18,7 @@ def join_l(l, sep):
 
 def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, centered: bool = False) -> str:
     """
-    :param rows: 2D list containing objects that have a single-line representation (via `str`).
-    All rows must be of the same length.
+    :param rows: 2D list containing objects that have a single-line representation (via `str`). All rows must be of the same length.
     :param labels: List containing the column labels. If present, the length must equal to that of each row.
     :param centered: If the items should be aligned to the center, else they are left aligned.
     :return: A table representing the rows passed in.
@@ -21,108 +26,106 @@ def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, center
 
     # Setup ################################################
 
-    debug = False
+    debug = False  # this is for troubleshooting code
 
-    consolidated = copy.deepcopy(rows[:])
+    consolidated: List[Any] = copy.deepcopy(rows[:])  # copies source to new object
+
+    labels_temp: List[Any] = []
 
     if labels != None:
-        labels_temp = copy.deepcopy(labels[:])
+        labels_temp: list[Any] = copy.deepcopy(labels[:])
 
     if labels:
-        consolidated.insert(0,labels_temp)
+        consolidated.insert(0, labels_temp)
     if debug:
         print(consolidated)
 
-    width = len(consolidated[0])
+    width_of_grid: int = len(consolidated[0])
 
     # Find Largest ##########################################
 
-    largest_entry = ['']*width
+    largest_entry: list[str] = [""] * width_of_grid
 
     for i in consolidated:
-        for x in range(0, width):
+        for x in range(0, width_of_grid):
             a = len(str(i[x]))
             b = len(largest_entry[x])
-            if (a > b):
+            if a > b:
                 largest_entry[x] = str(i[x])
 
     # Adjust Entries to Match Largest ###############################
 
-    for RowIndex, x in enumerate(consolidated):
-        for ColIndex, y in enumerate(x):
+    for row_index, x in enumerate(consolidated):
+        for col_index, y in enumerate(x):
+            
             if y is not int:
                 y = str(y)
-            if len(y) < len(str(largest_entry[ColIndex])):
+
+            if len(y) < len(str(largest_entry[col_index])):
                 if centered:
                     # centered
-                    consolidated[RowIndex][ColIndex] = y.center(
-                        len(largest_entry[ColIndex]), ' ')
+                    consolidated[row_index][col_index] = y.center(len(largest_entry[col_index]), " ")
                 else:
                     # left adjusted
-                    consolidated[RowIndex][ColIndex] = y.ljust(
-                        len(largest_entry[ColIndex]), ' ')
+                    consolidated[row_index][col_index] = y.ljust(len(largest_entry[col_index]), " ")
 
-    RowsJoined = []
+    rows_joined = []
     for index, x in enumerate(consolidated):
-        RowsJoined.append("│ " + join_l(consolidated[index], " │ ") + " │")
+        rows_joined.append("│ " + join_l(consolidated[index], " │ ") + " │")
 
     # Make Top and Bottom ##########################################
-
     # ┌────────────┬───────────┬─────────┐
     top = []
-    for i in range(width):
-        top.append("─"*(len(largest_entry[i])+2) + "┬")
-    top[width-1] = top[width-1].replace('┬', '')
+    for i in range(width_of_grid):
+        top.append("─" * (len(largest_entry[i]) + 2) + "┬")
+    top[width_of_grid - 1] = top[width_of_grid - 1].replace("┬", "")
+
     if debug:
-        print('┌'+''.join(top)+'┐')
+        print("┌" + "".join(top) + "┐")
 
     # └────────────┴───────────┴─────────┘
     bottom = []
-    for i in range(width):
-        bottom.append("─"*(len(largest_entry[i])+2) + "┴")
-    bottom[width-1] = bottom[width-1].replace('┴', '')
+    for i in range(width_of_grid):
+        bottom.append("─" * (len(largest_entry[i]) + 2) + "┴")
+    bottom[width_of_grid - 1] = bottom[width_of_grid - 1].replace("┴", "")
+
     if debug:
-        print('└'+''.join(bottom)+'┘')
+        print("└" + "".join(bottom) + "┘")
 
+    # Make Seperator ##########################################
     # ├────────────┼───────────┼─────────┤
-
+    seperator: list[Any] = []
     if labels != None:
-        seperator = []
+        for i in range(width_of_grid):
+            seperator.append("─" * (len(largest_entry[i]) + 2) + "┼")
+        seperator[width_of_grid - 1] = seperator[width_of_grid - 1].replace("┼", "")
 
-        for i in range(width):
-            seperator.append("─"*(len(largest_entry[i])+2) + "┼")
-        seperator[width-1] = seperator[width-1].replace('┼', '')
         if debug:
-            print('├'+''.join(seperator)+'┤')
+            print("├" + "".join(seperator) + "┤")
 
-    RowsJoined.insert(0, '┌'+''.join(top)+'┐')
+    rows_joined.insert(0, "┌" + "".join(top) + "┐")
     if labels:
-        RowsJoined.insert(2, '├'+''.join(seperator)+'┤')
-    RowsJoined.append('└'+''.join(bottom)+'┘')
-    #################################################################
+        rows_joined.insert(2, "├" + "".join(seperator) + "┤")
+    rows_joined.append("└" + "".join(bottom) + "┘")
 
     # Final #########################################################
 
-    for i in RowsJoined:
+    for i in rows_joined:
         if debug:
             print(i)
 
-    output = '\n'.join(RowsJoined)
-
-    #################################################################
+    output = "\n".join(rows_joined)
 
     if debug:
-        print(f'labels: {labels}')
+        print(f"labels: {labels}")
     if debug:
-        print(f'rows:  {rows}')
+        print(f"rows: {rows}")
 
     return output
 
 
 if __name__ == "__main__":
-    rows = [
-        ["Apple", 5, 70]
-    ]
+    rows = [["Apple", 5, 70]]
     labels = ["Fruit", "Tastiness", "Sweetness"]
     table = make_table(rows, labels)
 
--- qualifier/solution.py
+++ qualifier/solution.py
@@ -1,33 +1,54 @@
-from typing import Any, List, Optional
-from terminaltables import SingleTable
+from typing import Any, List, Tuple, TypeVar, Iterator, Optional
 
-def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, centered: bool = False) -> Optional[str]:
+T = TypeVar("T")
 
-    example_table = rows
-    example_table.insert(0,labels)
 
-    table_instance = SingleTable(example_table)
-    if centered:
-        for idx, i in enumerate(labels):
-            table_instance.justify_columns[idx] = 'center'
-            print(idx)
-    print(table_instance.table)
+def transpose(*rows: List[T]) -> Iterator[T]:
+    """Transpose a list of rows into a list of columns or vice-versa."""
+    return map(list, zip(*rows))
 
-    return None
 
-if __name__ == "__main__":
+def row_with_separators(separators: Tuple[str, str, str], row: Tuple[str, ...]) -> str:
+    left, middle, right = separators
+    return f'{left}{middle.join(map(str, row))}{right}'
 
-    rows = [
-        ["Apple", 5, 70, "Red", 76],
-        ["Banana", 3, 5, "Yellow", 8],
-        ["Cherry", 7, 31, "Red", 92],
-        ["Kiwi", 4, 102, "Green", 1],
-        ["Strawberry", 6, 134, "Red", 28]
-    ]
-    labels = ["Fruit", "Tastiness", "Sweetness", "Colour", "Smell"]
-    centered = False
 
-    table = make_table(rows, labels, centered)
-    print(table)
+def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, centered: bool = False) -> str:
+    """
+    :param rows: 2D array containing object that can be converted to string using `str(obj)`.
+    :param labels: Array containing the column labels, the length must equal that of rows.
+    :param centered: If the items should be aligned to the center, else they are left aligned.
+    :return: A table representing the rows passed in.
+    """
+    # Transpose into columns
+    columns = list(transpose(labels, *rows) if labels else transpose(*rows))
 
-    
+    # Padding
+    for column in columns:
+        # Find the required column width
+        column_width = max(map(len, map(str, column)))
+
+        # Add and record padding
+        for i, item in enumerate(column):
+            column[i] = f' {str(item):^{column_width}} ' if centered else f' {str(item):<{column_width}} '
+
+    # Border Widths
+    horizontal_lines = tuple('─' * len(column[0]) for column in columns)
+
+    # Create a list of rows with the row separators
+    rows = [row_with_separators(('│', '│', '│'), row) for row in transpose(*columns)]
+
+    # Create a separator between the labels and the values if needed
+    if labels:
+        label_border_bottom = row_with_separators(('├', '┼', '┤'), horizontal_lines)
+        rows.insert(1, label_border_bottom)
+
+    # Create the top and bottom border of the table
+    top_border = row_with_separators(('┌', '┬', '┐'), horizontal_lines)
+    rows.insert(0, top_border)
+
+    bottom_border = row_with_separators(('└', '┴', '┘'), horizontal_lines)
+    rows.append(bottom_border)
+
+    # Join all the components
+    return '\n'.join(rows)
